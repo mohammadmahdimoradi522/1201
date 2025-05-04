1f011ddba91ba771e63e505b399afcac32cae30c
mohammadmahdimoradi522 <mamadgemer7@gmail.com>
2025-05-04 21:32:13 +0330

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dlogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string username = textBox2.Text;
+            int password = Convert.ToInt32(textBox3.Text);
+
+
+            string path = "save.txt";
+            string all = $"name:{name} , username:{username} , password:{password}";
+
+            try
+            {
+                File.AppendAllText(path, all + Environment.NewLine);
+                MessageBox.Show("ok");
+
+                
+                Form1 form1 = new Form1();
+                form1.Show();
+
+                
+                this.Hide();
+            }
+            catch
+            {
+                MessageBox.Show("!wrong");
+            }
+        }
+
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar)) 
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
