b039fbb0563d18206c1eb640832153bb96c3d6d9
mohammadmahdimoradi522 <mamadgemer7@gmail.com>
2025-05-10 17:21:41 +0330

    Add files via upload

---
+++ phone numbers.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataDataSet.product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.dataDataSet.product);
+            // TODO: This line of code loads data into the 'database2DataSet.Members' table. You can move, or remove it, as needed.
+            this.membersTableAdapter.Fill(this.database2DataSet.Members);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Delet_Click(object sender, EventArgs e)
+        {
+            if (productTableAdapter == null)
+            {
+                MessageBox.Show("مقداری وارد نشده است");
+            }
+            else 
+            {
+                this.productTableAdapter.DeleteQuery(Convert.ToInt32(textBox1.Text));
+                this.productTableAdapter.Fill(this.dataDataSet.product);
+            }
+                
+
+            
+        }
+
+
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            this.productTableAdapter.InsertQuery1(Convert.ToInt32(textBox1.Text),textBox2.Text,Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text));
+            this.productTableAdapter.Fill(this.dataDataSet.product);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            this.productTableAdapter.UpdateQuery(Convert.ToInt32(textBox1.Text), textBox2.Text, Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text));
+            this.productTableAdapter.Fill(this.dataDataSet.product);
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        
+
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void productBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            if (textBox5.Text == "")
+            {
+                this.productTableAdapter.Fill(this.dataDataSet.product);
+            }
+            else
+            {
+
+                this.productTableAdapter.FillByname(dataDataSet.product, textBox5.Text);
+
+            }
+        }
+    }
+}
