55480a9253f6c1ff6df3c54ffe8bd51102117ef8
mohammadmahdimoradi522 <mamadgemer7@gmail.com>
2025-05-10 17:20:16 +0330

    Add files via upload

---
+++ login.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dlogin
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txt_name_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_usernaem_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_password_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            string name = txt_name.Text;
+            string username = txt_username.Text;
+            int password = Convert.ToInt32(txt_password.Text);
+
+
+            string path = "save.txt";
+            string all = $"name:{name} , username:{username} , password:{password}";
+
+            try
+            {
+                File.AppendAllText(path, all + Environment.NewLine);
+                MessageBox.Show("ok");
+
+
+                login form1 = new login();
+                form1.Show();
+
+
+                this.Hide();
+            }
+            catch
+            {
+                MessageBox.Show("!wrong");
+            }
+        }
+    }
+}
